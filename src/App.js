8976400cf34142bdd0112bf595f142ea39dca004
Rached Masri <masri.rached@gmail.com>
2022-02-04 11:37:44 +0100

    Fix Delete FeedbackItem

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App () {
     const [feedback, setFeedback ] = useState (FeedbackData)
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete ?')){
-            setFeedback(feedback.filter)((item) => item.id !== id)
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
     return(
