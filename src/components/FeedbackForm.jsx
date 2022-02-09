af49f86bc2d106cf103b765d6b9e02919c06765d
Rached Masri <masri.rached@gmail.com>
2022-02-09 12:24:35 +0100

    Create Rating Select Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
+
 
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+
 
     const handleTextChange = (e) => {
-
+        if (text === '') {
+          setBtnDisabled(true)
+          setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+          setMessage('Text must be at least 10 characters')
+          setBtnDisabled(true)
+        } else {
+          setMessage(null)
+          setBtnDisabled(false)
+        }
+    
         setText(e.target.value)
       }
 
@@ -16,6 +32,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
@@ -23,11 +40,12 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit'>
+          <Button type='submit'  isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
 
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
 )
