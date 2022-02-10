6eb576580ff5986569f30dfa5bcf67a83808eb9b
Rached Masri <masri.rached@gmail.com>
2022-02-10 23:15:23 +0100

    Add Feedback

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/FeedbackForm"
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid" 
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
-function App () {
-    const [feedback, setFeedback ] = useState (FeedbackData)
-    const deleteFeedback = (id) => {
-        if (window.confirm('Are you sure you want to delete ?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+      newFeedback.id = uuidv4()
+      setFeedback([newFeedback, ...feedback])
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete ?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-    return(
-        <>
-        <Header/>
-        <div className="container">
-            <FeedbackForm/>
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-        </div>
-        </>
-    )
+  };
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
